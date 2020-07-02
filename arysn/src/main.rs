bdd6cbfc76d204f6ec19344e5ffd9d604136f536
Tahara Yoshinori <read.eval.print@gmail.com>
2020-07-02 20:15:35 +0900

    query してみたり

--- arysn/src/main.rs
+++ arysn/src/main.rs
@@ -8,37 +8,39 @@ mod tests {
     use arysn_macro::defar;
     use tokio_postgres::{Error, NoTls};
 
-    defar!(Neko);
+    defar!(User);
 
     #[tokio::test]
     async fn it_works() -> Result<(), Error> {
-        let neko = Neko {
+        let user = User {
             name: "こねら".to_string(),
         };
-        println!("{}!!!!", &neko.name);
-        assert_eq!("こねら", &neko.name);
+        println!("{}!!!!", &user.name);
+        assert_eq!("こねら", &user.name);
 
         let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set!");
-
-        // Connect to the database.
         let (client, connection) = tokio_postgres::connect(&database_url, NoTls).await?;
-
-        // The connection object performs the actual communication with the database,
-        // so spawn it off to run on its own.
         tokio::spawn(async move {
             if let Err(e) = connection.await {
                 eprintln!("connection error: {}", e);
             }
         });
-
         client.execute("SET log_statement = 'all'", &[]).await?;
         client.execute("SET TIME ZONE 'Japan'", &[]).await?;
-        // Now we can execute a simple statement that just returns its parameter.
-        let rows = client.query("SELECT $1::TEXT", &[&"hello world"]).await?;
 
-        // And then check that we got back the same string we sent over.
+        let rows = client.query("SELECT $1::TEXT", &[&"hello world"]).await?;
         let value: &str = rows[0].get(0);
         assert_eq!(value, "hello world");
+
+        let rows = client
+            .query("SELECT id, name FROM users where id=$1", &[&1i64])
+            .await?;
+        println!("rows {:?}", &rows);
+        let id: i64 = rows[0].get(0);
+        assert_eq!(id, 1);
+        let value: &str = rows[0].get(1);
+        assert_eq!(value, "ユーザ1");
+
         Ok(())
     }
 }
