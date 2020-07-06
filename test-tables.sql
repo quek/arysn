1b9f2b4c5a4079a9007175082708c7027d10b530
Tahara Yoshinori <read.eval.print@gmail.com>
2020-07-06 19:33:48 +0900

    テスト用テーブル

--- test-tables.sql
+++ test-tables.sql
@@ -1,6 +1,7 @@
 SET log_statement = 'all';
 SET TIME ZONE 'Japan';
 
+DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -16,4 +17,16 @@ INSERT INTO users(name, title, age, active, created_at) VALUES
  ('ユーザ1', '旅人', 20, TRUE, CURRENT_TIMESTAMP)
 ,('ユーザ2', NULL, 21, FALSE, CURRENT_TIMESTAMP)
 ,('ユーザ3', 'もののけ', 22, TRUE, CURRENT_TIMESTAMP)
+;
+
+CREATE TABLE roles (
+  id BIGSERIAL PRIMARY KEY,
+  user_id BIGINT NOT NULL REFERENCES users ON DELETE CASCADE,
+  name VARCHAR(255) NOT NULL
+);
+
+INSERT INTO roles(user_id, name) VALUES
+ (1, '管理')
+,(1, '編集')
+,(2, '参照')
 ;
