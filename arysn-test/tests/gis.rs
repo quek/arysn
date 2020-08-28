3aa63bd1e13b6297dd3703cd8b0ea673dcfac51e
Tahara Yoshinori <read.eval.print@gmail.com>
2020-08-28 17:12:48 +0900

    rust-postgis を使う

--- arysn-test/tests/gis.rs
+++ arysn-test/tests/gis.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use arysn::prelude::*;
-use arysn_test::generated::gis_thing::GisThing;
+use arysn_test::generated::gis_thing::{GisThing, GisThingNew};
 use common::init;
 
 mod common;
@@ -11,11 +11,35 @@ async fn gis() -> Result<()> {
     let mut conn = connect().await?;
     let conn = &conn.transaction().await?;
 
-    let things = GisThing::select().load(&conn).await?;
+    let thing = GisThing::select()
+        .order()
+        .id()
+        .asc()
+        .limit(1)
+        .first(&conn)
+        .await?;
+    assert_eq!(thing.latlng.x, 139.71019683);
+    assert_eq!(thing.latlng.y, 35.66432955);
+
+    GisThingNew {
+        id: None,
+        latlng: postgis::ewkb::Point::new(139.7101234, 35.6641234, None),
+    }
+    .insert(&conn)
+    .await?;
+    let thing = GisThing::select()
+        .order()
+        .id()
+        .desc()
+        .limit(1)
+        .first(&conn)
+        .await?;
     println!(
-        "=============================================================================\n{:#?}",
-        things
+        "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n {} {}",
+        thing.latlng.x, thing.latlng.y
     );
+    assert_eq!(thing.latlng.x, 139.7101234);
+    assert_eq!(thing.latlng.y, 35.6641234);
 
     Ok(())
 }
