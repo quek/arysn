ab9fd7aafae76e250cb5c94d74f07132697e140f
Tahara Yoshinori <read.eval.print@gmail.com>
2021-07-30 08:45:10 +0900

    cfg gis

--- arysn/src/lib.rs
+++ arysn/src/lib.rs
@@ -1,32 +1,34 @@
 #[cfg(feature = "with-tokio-0_2")]
-extern crate bytes_0_5 as bytes;
-#[cfg(feature = "with-tokio-0_2")]
 extern crate deadpool_0_5 as deadpool;
 #[cfg(feature = "with-tokio-0_2")]
 extern crate deadpool_postgres_0_5 as deadpool_postgres;
-#[cfg(feature = "with-tokio-0_2")]
-extern crate postgis_0_7 as postgis;
-#[cfg(feature = "with-tokio-0_2")]
-extern crate postgres_0_17 as postgres;
 #[cfg(feature = "with-tokio-0_2")]
 extern crate tokio_0_2 as tokio;
 #[cfg(feature = "with-tokio-0_2")]
 extern crate tokio_postgres_0_5 as tokio_postgres;
 
 #[cfg(feature = "with-tokio-1_x")]
-extern crate bytes_1_x as bytes;
-#[cfg(feature = "with-tokio-1_x")]
 extern crate deadpool_0_8 as deadpool;
 #[cfg(feature = "with-tokio-1_x")]
 extern crate deadpool_postgres_0_9 as deadpool_postgres;
 #[cfg(feature = "with-tokio-1_x")]
-extern crate postgis_0_8 as postgis;
-#[cfg(feature = "with-tokio-1_x")]
-extern crate postgres_0_19 as postgres;
-#[cfg(feature = "with-tokio-1_x")]
 extern crate tokio_1_x as tokio;
 #[cfg(feature = "with-tokio-1_x")]
 extern crate tokio_postgres_0_7 as tokio_postgres;
+
+#[cfg(feature = "gis-tokio-0_2")]
+extern crate bytes_0_5 as bytes;
+#[cfg(feature = "gis-tokio-0_2")]
+extern crate postgis_0_7 as postgis;
+#[cfg(feature = "gis-tokio-0_2")]
+extern crate postgres_0_17 as postgres;
+
+#[cfg(feature = "gis")]
+extern crate bytes_1_x as bytes;
+#[cfg(feature = "gis")]
+extern crate postgis_0_8 as postgis;
+#[cfg(feature = "gis")]
+extern crate postgres_0_19 as postgres;
 
 mod builder;
 mod db;
@@ -34,7 +36,7 @@ mod error;
 mod filter;
 mod filter_builder;
 pub mod generator;
-#[cfg(feature = "gis")]
+#[cfg(any(feature = "gis", feature = "gis-tokio-0_2"))]
 mod gis;
 mod order_item;
 mod value;
