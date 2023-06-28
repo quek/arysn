aefcb631fcf1d402de32e5a49caff25290383c66
Tahara Yoshinori <read.eval.print@gmail.com>
2023-06-28 09:21:01 +0900

    Fix r#where, or

--- arysn-test/tests/belongs_to.rs
+++ arysn-test/tests/belongs_to.rs
@@ -11,10 +11,12 @@ async fn belongs_to() -> Result<()> {
     let mut conn = connect().await?;
     let conn = &conn.transaction().await?;
     let roles = Role::select()
-        .user(|user| user.id().eq(1))
+        .user(|user| user.id().eq(1).preload())
         .load(conn)
         .await?;
     assert_eq!(roles.len(), 2);
+    assert_eq!(roles[0].user.as_ref().map(|x| x.id), Some(1));
+    assert_eq!(roles[1].user.as_ref().map(|x| x.id), Some(1));
 
     let roles = Role::select()
         .user(|user| user.preload())
@@ -37,6 +39,23 @@ async fn belongs_to() -> Result<()> {
     assert_eq!(user.create_projects[0].create_user().name, "ユーザ1");
     assert_eq!(user.create_projects[0].update_user().name, "ユーザ2");
     assert_eq!(user.create_projects[0].check_user, None);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn multiple_belongs_to() -> Result<()> {
+    init();
+    let mut conn = connect().await?;
+    let conn = &conn.transaction().await?;
+    let roles = Role::select()
+        .user(|user| user.preload().id().eq(2)) // Role の SELECT に使われない
+        .user(|user| user.id().eq(1)) // Role の SELECT に使われる
+        .load(conn)
+        .await?;
+    assert_eq!(roles.len(), 2);
+    assert_eq!(roles[0].user, None);
+    assert_eq!(roles[1].user, None);
 
     Ok(())
 }
